57cf4da94caa2d41917f1b6abd7d2637188a40f6
Paulo Prates <paulo.prates01@hotmail.com>
2024-05-05 23:37:14 -0300

    upload dos arquivos da atividade

---
+++ formulario-cadastro/MinhaAplicacao/src/factory/TestaConexão.java
@@ -0,0 +1,12 @@
+package factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaConexão {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = new ConnectionFactory().getConnection();
+        System.out.println("Conexao aberta!");
+        connection.close();
+    }
+}
